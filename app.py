1b3ac9d3ed49ee8d0255019ce0e0571f9a598ae8
jiarui.luo <291979254@qq.com>
2020-10-09 16:57:58 +0800

    auto send weather to multi person

--- app.py
+++ app.py
@@ -10,24 +10,17 @@ import requests
 import json
 import csv
 import datetime
+import json
+from mail import SendMail
 
-from mail import automail
-
-KEY = "ac123eeb7c16456a87a5ec3d322f09ff"
-CITY = "彭山"
-APIURL = "https://free-api.heweather.net/s6/weather/forecast"
-USERNAME = ""
-receiver = 'qiuyujie23@163.com'
-
-
-# automail(title,msg,receiver)
 
 class HeWeather(object):
-    def __init__(self):
-        pass
+    def __init__(self, api_url, api_key):
+        self.api_url = api_url  # api的url
+        self.api_key = api_key  # api的key
 
-    def get_data(self):
-        url = APIURL + '?location= ' + CITY + '&key=' + KEY
+    def get_data(self, location):
+        url = self.api_url + '?location= ' + location + '&key=' + self.api_key
         res = requests.get(url)
         res = json.loads(res.text)  # Change the response to json.
         status = res['HeWeather6'][0]['status']  # api status.
@@ -35,7 +28,10 @@ class HeWeather(object):
             return
         forecast = res['HeWeather6'][0]['daily_forecast']
         location = res['HeWeather6'][0]['basic']
-        text = {'city': location['parent_city'] + location['location']}  # add city key.
+        if location['parent_city'] == location['location']:
+            text = {'city': location['parent_city']}
+        else:
+            text = {'city': location['parent_city'] + location['location']}
         msg = []
         for data in forecast:
             date = data['date']
@@ -58,12 +54,54 @@ class HeWeather(object):
         return text
 
 
-if __name__ == '__main__':
-    he_weather = HeWeather()
-    weather_forecast = he_weather.get_data()
-    print(weather_forecast)
+def send_weather_to_email():
+    """
+        {
+          "API": {
+            "key": "*****************",
+            "url": "https://free-api.heweather.net/s6/weather/forecast"
+          },
+          "SENDER": {
+            "host" : "****",
+            "username" : "****",
+            "pwd" : "******"
+          },
+          "RECEIVER": [{
+            "username":"****",
+            "email": "*****",
+            "location": "**"
+          }]
+        }
+    :return:
+    """
+    # 读取配置文件
+    with open('config.json', 'r', encoding='utf-8') as f:
+        text = f.read()
+    paras = json.loads(text)
 
-    title = f"{weather_forecast['city']}未来3日天气预报，请查收！"
-    msg = str(weather_forecast)
-    receiver = 'jiarui.luo@hirain.com'
-    automail(title, msg, receiver)
+    # 初始化天气预报数据读取
+    he_weather = HeWeather(paras['API']['url'], paras['API']['key'])
+
+    # 初始化邮件发送模块
+    send_mail = SendMail(host=paras['SENDER']['host'],
+                         user=paras['SENDER']['username'],
+                         pwd=paras['SENDER']['pwd'])
+    # 获取所有的接收者
+    receivers = paras['RECEIVER']
+    for receiver in receivers:
+        weather_forecast = he_weather.get_data(receiver['location'])
+        title = f"{weather_forecast['city']}未来3日天气预报，请查收！"
+        days_weather = ""
+        for day in weather_forecast['weather_forecast']:
+            days_weather += f"<h3>{day['date']}， 天气{day['cond_txt_d']}， 最高温度{day['tmp_max']}，" \
+                            f"最低温度{day['tmp_min']}，湿度{day['hum']}， 风向{day['wind_dir']}，" \
+                            f"风力{day['wind_sc']}级，风速{day['wind_spd']}m/s</h3>"
+        msg = f"<h2>{receiver['username']}您好，{receiver['location']}未来三日的天气预报</h2><hr>" \
+              + days_weather + "<br><br><br><hr><small>该服务由程序自动发送</small>"
+        # 使用邮箱发送
+        if not send_mail.to(title, msg, receiver['email']):
+            break
+
+
+if __name__ == '__main__':
+    send_weather_to_email()
--- mail.py
+++ mail.py
@@ -2,45 +2,47 @@
 """
 @version: 1.0
 @author: Jarrett
-@file: sendemail
+@file: send_email
 @time: 2020/2/18 15:59
 """
 
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from email.header import Header
 from email.utils import formataddr
 
-HOST = 'smtp.163.com'
-USER = '*************@163.com'
-PWD = '#############'
 
-def automail(title, msg, receivers):
-    """
+class SendMail(object):
+    def __init__(self, host, user, pwd):
+        self.host = host  # 设置服务器
+        self.user = user  # 用户名
+        self.pwd = pwd  # 口令
+        pass
 
-    :param title:
-    :param msg:
-    :param receivers: str, 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-    :return:
-    """
-    # 第三方 SMTP 服务
-    mail_host = HOST  # 设置服务器
-    mail_user = USER  # 用户名
-    mail_pass = PWD  # 口令
+    def to(self, title, msg, receivers):
+        """
+        第三方 SMTP 服务
+        :param title:
+        :param msg:
+        :param receivers: str, 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+        :return:
+        """
+        message = MIMEMultipart()
+        mail_body = MIMEText(msg, _subtype='html', _charset='utf-8')
+        message['From'] = formataddr(["Jarrett", self.user])  # 括号里对应收件人邮箱昵称发件人邮箱账号
+        message['To'] = formataddr([receivers, receivers])
+        # 邮件主题，也就是说是标题
+        message['Subject'] = Header(title, 'utf-8')
+        message.attach(mail_body)
 
-    # 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
-    message = MIMEText(msg, 'plain', 'utf-8')
-    message['From'] = formataddr(["Jarrett", mail_user])  # 括号里对应收件人邮箱昵称发件人邮箱账号
-    message['To'] = formataddr([receivers, receivers])
-
-    subject = title  # 邮件主题，也就是说是标题
-    message['Subject'] = Header(subject, 'utf-8')
-
-    try:
-        sever = smtplib.SMTP(mail_host, 25)  # 发件人邮箱中的SMTP服务器
-        sever.login(mail_user, mail_pass)
-        sever.sendmail(mail_user, [receivers], message.as_string())  # sendmail的第二个参数必须是list，可以发送多个用户
-        print("邮件发送成功")
-        sever.quit()  # 关闭连接
-    except smtplib.SMTPException:
-        print("Error: 无法发送邮件至" + receivers)
+        try:
+            sever = smtplib.SMTP(self.host, 25)  # 发件人邮箱中的SMTP服务器
+            sever.login(self.user, self.pwd)
+            sever.sendmail(self.user, [receivers], message.as_string())  # sendmail的第二个参数必须是list，可以发送多个用户
+            print("邮件发送成功")
+            sever.quit()  # 关闭连接
+            return True
+        except smtplib.SMTPException:
+            print("Error: 无法发送邮件至" + receivers)
+            return False
